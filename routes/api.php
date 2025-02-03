12ca7886c7fa7a0bc131898a453e5d9b50575d19
Pagonoudis <giorgospago23@gmail.com>
2025-02-03 20:42:23 +0200

    first commit

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'Welcome to e-food API'
+    ]);
+});
+
+Route::prefix('admin')->group(base_path('routes/admin.php'));
+Route::prefix('merchant')->group(base_path('routes/merchant.php'));
+Route::prefix('driver')->group(base_path('routes/driver.php'));
+Route::prefix('client')->group(base_path('routes/client.php'));
