d360572c0640a4e6838e4dab354b7f575eb977fe
Pagonoudis <giorgospago23@gmail.com>
2025-02-05 11:58:11 +0200

    Add users controller

--- routes/client.php
+++ routes/client.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return response()->json([
@@ -11,5 +12,9 @@ Route::get('/', function () {
 Route::prefix('auth')->group(base_path('routes/auth.php'));
 
 Route::middleware(['auth:sanctum'])->group(function() {
-
+    Route::prefix("users")->group(function(){
+        Route::get("me", [UserController::class, 'me']);
+        Route::get("tokens", [UserController::class, 'tokens']);
+        Route::delete("revoke-all-tokens", [UserController::class, 'revokeAllTokens']);
+    });
 });
