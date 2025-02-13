29b7d0bb7abba4c4cb522ee5ac58e2c7aded6a48
Lefteris Panelas <panelasajax@gmail.com>
2025-02-13 11:26:45 +0200

    Add addresses

--- routes/client.php
+++ routes/client.php
@@ -14,9 +14,10 @@ Route::prefix('auth')->group(base_path('routes/auth.php'));
 Route::middleware(['auth:sanctum'])->group(function() {
     Route::prefix("users")->group(function(){
         Route::get("me", [UserController::class, 'me']);
+        Route::get("addresses", [UserController::class, 'addresses']);
         Route::get("tokens", [UserController::class, 'tokens']);
         Route::delete("revoke-all-tokens", [UserController::class, 'revokeAllTokens']);
     });
 
-    
+
 });
