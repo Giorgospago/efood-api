a8f9e49b260a63bfaa18f7e1041a5e405af61bfb
Pagonoudis <giorgospago23@gmail.com>
2025-02-24 20:45:09 +0200

    test

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return response()->json([
-        'message' => 'Welcome to e-food API !'
+        'message' => 'Welcome to e-food API !!!'
     ]);
 });
 
