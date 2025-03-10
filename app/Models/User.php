975839d32f461fcfe33210c354e27b6a14edeccd
Pagonoudis <giorgospago23@gmail.com>
2025-03-10 21:44:56 +0200

    Add Orders and order products and coupons

--- app/Models/User.php
+++ app/Models/User.php
@@ -76,4 +76,15 @@ class User extends Authenticatable implements FilamentUser, MustVerifyEmail
     {
         return $this->hasMany(Address::class);
     }
+
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class);
+    }
+
+    public function stores(): HasMany
+    {
+        return $this->hasMany(Store::class);
+    }
+    
 }
